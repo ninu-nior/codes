f85956827697831a2a5780c6da4133abffe310e8
Nehal Mantri <nehalsmantri@gmail.com>
2024-05-03 06:55:49 +0530

    f

---
+++ firstbootloader.asm
@@ -0,0 +1,7 @@
+[BITS 16]       ;tell the assembler that its a 16 bit code
+[ORG 0x7C00]    ;Origin, tell the assembler that where the code willbe in memory after it is been loaded
+
+JMP $           ;infinite loop
+
+TIMES 510 - ($ - $$) db 0       ;fill the rest of sector with 0
+DW 0xAA55                       ; add boot signature at the end of bootloader
